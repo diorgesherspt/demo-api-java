7583cb8f08fd43643c9838e27041f0f9fbaf17e4
unknown <diorgesherstp@outlook.com.br>
2023-09-24 14:05:14 -0300

    initial commit

---
+++ src/main/java/com/example/demo/Controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping
+    public String welcome(){
+        return "hello world";
+    }
+}
